c0990806f149d5194875908aa5bf5dd1c1074928
Neemo Dab <neemodab@gmail.com>
2022-06-17 11:04:49 +0100

    created react app, set up components folder, started to plan cat button component

---
+++ cats-are-life/src/components/App/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CATS ARE LIFE!</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ cats-are-life/src/components/CatButton/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//PLAN
+//create function CatButton({handleClick}){
+// logic creating button 
+// event listener handleClick which is a prop 
+// async (await) fetch request to cat api 
+//return <button/> 
+//display image may need to be a seperate component
+//}
