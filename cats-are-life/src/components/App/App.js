2cc78b8ac42344f6da2faa163c1a6d0e243b2be5
Neemo Dab <neemodab@gmail.com>
2022-06-17 14:42:10 +0100

    button is now working and fetching onClick

--- cats-are-life/src/components/App/App.js
+++ cats-are-life/src/components/App/App.js
@@ -1,16 +1,24 @@
 
 import './App.css';
 import CatButton from '../CatButton';
+import { useState } from 'react';
 
-function handleClick() {
-  CatButton();
-}
 
 function App() {
-  return (
+  const [currentCats, fetchCats]= useState(false)
+  function handleClick() {
+    // currentCats === true ? 
+    // fetchCats(false) : fetchCats(true) 
+    if(currentCats === true){
+      fetchCats(false)
+    }else{
+      fetchCats(true)
+    }
+  }
+  return(
     <div className="App">
       <h1>CATS ARE LIFE!</h1>
-      <CatButton />
+      <CatButton getCats={currentCats}/>
       <button className="cat-button" onClick={handleClick}>GET ME A CAT FOR MY LIFE</button>
     </div>
   );
--- cats-are-life/src/components/CatButton/index.js
+++ cats-are-life/src/components/CatButton/index.js
@@ -1,48 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 //PLAN
 //create function CatButton({handleClick}){
-// logic creating button 
-// event listener handleClick which is a prop 
-// async (await) fetch request to cat api 
-//return <button/> 
+// logic creating button
+// event listener handleClick which is a prop
+// async (await) fetch request to cat api
+//return <button/>
 //display image may need to be a seperate component
 //}
 
+function CatButton({ getCats }) {
+  const [cats, setCats] = useState([]);
 
-function CatButton() {
-    const [cats, setCats]= useState([{
-        "breeds": [
-          
-        ],
-        "id": "MTQ5NjUwNQ",
-        "url": "https://cdn2.thecatapi.com/images/MTQ5NjUwNQ.jpg",
-        "width": 4272,
-        "height": 2848
-      }]);
-    
-    useEffect(function () {
-        const fetchData = async function() {
-            const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
-            const data = await response.json();
-            setCats(data);
-        }
-            
-        fetchData()
-        }, []);//if no dependency OR dependency [cats] app loads without errors but is stuck in infite loop of getting cats
-      
-
-    let exists = false;
-    if (cats[0].url) {
-        exists = true;
-    }
-
-    return (
-        <>
-            {exists && <img src={cats[0].url} alt="MEOW"></img>}
-
-        </>
+  useEffect(
+    function () {
+      const fetchData = async function () {
+        const response = await fetch(
+          `https://api.thecatapi.com/v1/images/search`
         );
+        const data = await response.json();
+        setCats(data[0].url);
+      };
 
+      fetchData();
+    },
+    [getCats]
+  ); //if no dependency OR dependency [cats] app loads without errors but is stuck in infite loop of getting cats
+
+  //
+  // let exists = false;
+  // if (cats[0].url) {
+  // exists = true;
+  // }
+
+  return (
+    <>
+      <img src={cats} alt="MEOW"></img>
+    </>
+  );
 }
 
 export default CatButton;
