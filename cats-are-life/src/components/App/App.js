36e14c5892271855731512f6eb4503a74ba3e607
alicialowe <hohpascal@outlook.com>
2022-06-17 12:07:22 +0100

    CatButton function written with useEffect, button is displaying on screen but has no functionality yet, fetch request is working and visible in console but no screen display component written

--- cats-are-life/src/components/App/App.js
+++ cats-are-life/src/components/App/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import CatButton from '../CatButton';
 
 function App() {
   return (
     <div className="App">
       <h1>CATS ARE LIFE!</h1>
-      
+      <CatButton />
     </div>
   );
 }
--- cats-are-life/src/components/CatButton/index.js
+++ cats-are-life/src/components/CatButton/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 //PLAN
 //create function CatButton({handleClick}){
 // logic creating button 
@@ -7,3 +7,28 @@ import React from 'react';
 //return <button/> 
 //display image may need to be a seperate component
 //}
+
+function CatButton() {
+    const [cats, setCats]= useState([]);
+    
+    useEffect(function () {
+        const fetchData = async function() {
+            const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
+            const data = await response.json();
+            setCats(data);
+        }
+            
+        fetchData()
+        }, []);
+
+        console.log(cats);
+
+    return (
+        <>
+            <button className="cat-button">GET ME A CAT FOR MY LIFE</button>
+        </>
+        );
+
+}
+
+export default CatButton;
